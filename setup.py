e0b290ccee9a31c501de76cb9ec460c57adeae7f
vats <goktugcetin1@gmail.com>
2022-04-08 12:26:17 +0300

    v0.7.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="vats_proxy",
-    version="0.6.2",
+    version="0.7.1",
     description="Free Proxy Library for requests library",
     long_description=long_description,
     long_description_content_type="text/markdown",
